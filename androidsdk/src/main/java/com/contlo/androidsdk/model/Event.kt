1aa47c8cbb2ee94284ae5aa461c646591b907c20
AmanContlo <aman.toppo@contlo.com>
2023-09-20 12:04:02 +0530

    add event_id to event api

--- androidsdk/src/main/java/com/contlo/androidsdk/model/Event.kt
+++ androidsdk/src/main/java/com/contlo/androidsdk/model/Event.kt
@@ -1,8 +1,11 @@
 package com.contlo.androidsdk.model
 
 import com.google.gson.annotations.SerializedName
+import java.util.UUID
 
 data class Event(
+
+    @SerializedName("event_id") val eventId: String = generateId(),
     val event: String,
     @SerializedName("fcm_token") val fcmToken: String?,
     var email: String?,
@@ -15,5 +18,13 @@ data class Event(
         email = if(email.isNullOrBlank()) null else email
         phoneNumber = if(phoneNumber.isNullOrBlank()) null else phoneNumber
         return this
+    }
+
+    companion object {
+        private const val prefix = "event-"
+
+        private fun generateId(): String {
+            return prefix + UUID.randomUUID().toString()
+        }
     }
 }
